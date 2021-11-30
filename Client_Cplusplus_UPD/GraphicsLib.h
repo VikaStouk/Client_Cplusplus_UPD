bbb7e97096bbd594783bb4d888067e8ec7dda45f
Vika Stouk <vikastoukv@Gmail.com>
2021-11-30 18:49:14 +0200

    Внесены изменения в макросы GraphicsLib

--- Client_Cplusplus_UPD/GraphicsLib.h
+++ Client_Cplusplus_UPD/GraphicsLib.h
@@ -3,9 +3,9 @@
 
 #include <stdint.h>
 
-#ifndef RGB
+//#ifndef RGB
 #define RGB(r,g,b) (((r&0xF8)<<8)|((g&0xFC)<<3)|((b&0xF8)>>3)) //RGB565: 5 red | 6 green | 5 blue
-#endif
+//#endif
 
 
 class GraphicsLib
--- Client_Cplusplus_UPD/test.cpp
+++ Client_Cplusplus_UPD/test.cpp
@@ -25,17 +25,17 @@ int main(int argc, char** argv) {
 	
 
 
-	/*display.fillRect(50, 100, 10, 10, RGB(255, 5, 5));
+	display.fillRect(50, 100, 10, 10, RGB(255, 0, 0));
 	cout << "Command 5" << endl;
 	Sleep(500);
-	display.fillRect(100, 100, 20, 20, RGB(0, 120, 74));
+	display.fillRect(100, 100, 20, 20, RGB(0, 255, 0));
 	cout << "Command 5" << endl;
-	Sleep(500);*/
-	display.fillRect(150, 100, 30, 30, RGB(0, 255, 0));
+	Sleep(500);
+	display.fillRect(150, 100, 30, 30, RGB(0, 0, 255));
 	cout << "Command 5" << endl;
 	Sleep(500);
 	
-	/*display.drawLine(10, 150, 60, 150, RGB(0, 0, 0));
+	display.drawLine(10, 150, 60, 150, RGB(0, 0, 0));
 	cout << "Command 3" << endl;
 	Sleep(500);
 	display.drawLine(70, 150, 60, 150, RGB(0, 0, 0));
@@ -48,17 +48,17 @@ int main(int argc, char** argv) {
 	cout << "Command 3" << endl;
 	Sleep(500);
 
-	display.drawEllipse(50, 190, 10, 10, RGB(1, 0, 0));
+	display.drawEllipse(50, 190, 10, 10, RGB(255, 255, 0));
 	cout << "Command 6" << endl;
 	Sleep(500);
-	display.drawEllipse(100, 180, 20, 20, RGB(0, 0, 0));
+	display.drawEllipse(100, 180, 20, 20, RGB(255, 160, 0));
 	cout << "Command 6" << endl;
 	Sleep(500);
-	display.drawEllipse(150, 170, 30, 30, RGB(0, 0, 0));
+	display.drawEllipse(150, 170, 30, 30, RGB(255, 0, 255));
 	cout << "Command 6" << endl;
 	Sleep(500);
 
-	display.fillEllipse(210, 120, 50, 50, RGB(0, 0, 0));
+	display.fillEllipse(210, 120, 50, 50, RGB(0, 255, 255));
 	cout << "Command 6" << endl;
 	Sleep(500);
 	display.drawEllipse(210, 115, 60, 60, RGB(0, 0, 0));
@@ -66,7 +66,7 @@ int main(int argc, char** argv) {
 	Sleep(500);
 
 
-	display.drawText(10, 10, "Abstraction", RGB(0, 0, 0), RGB(0, 0, 0), 12);*/
+	display.drawText(10, 10, "Abstraction", RGB(0, 0, 0), RGB(0, 0, 0), 12);
 	getchar();
 	return 0;
 }
